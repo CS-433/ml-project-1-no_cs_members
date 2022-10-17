3f1251f1c273f2115866ec97e452cd07cbbc8980
iris263 <115156488+iris263@users.noreply.github.com>
2022-10-17 11:33:05 +0200

    Update implementations.py
    add gradient descent and least squared

--- implementations.py
+++ implementations.py
@@ -1,8 +1,52 @@
 
 from helpers_stochastic_gradient import*
+from helpers_gradient import*
 from helpers import*
 from costs import*
+import numpy as np
 
+def least_squares(y, tx):
+    """Calculate the least squares solution.
+       returns mse, and optimal weights.
+    
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+    
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar.
+
+    >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
+    (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # least squares: TODO
+    # returns mse, and optimal weights
+    N = y.shape[0]
+    w = np.linalg.solve(tx.T.dot(tx), tx.T.dot(y))
+    e = y - w.dot(tx.T)                    #mse = (1/(2*N)) * np.sum((y-w[0]-w[1]*tx[:,1])**2)
+    mse = (1/(2*N))*e.T.dot(e)
+    # ***************************************************
+    return w, mse
+
+def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma) : 
+    """Gradient descent algorithm."""
+    # Define parameters to store w and loss
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute the gradient 
+        grad = compute_gradient(y, tx, w)
+       
+        # update w by gradient descent
+        w = w - gamma * grad
+        # compute loss for this w
+        loss = compute_loss(y,tx,w)
+      
+
+    return w, loss
+    
 def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma) : 
     #Linear regression using stochastic gradient descent
     #Compute the stochastic gradient descent algorithm for one batch
