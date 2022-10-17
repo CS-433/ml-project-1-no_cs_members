7b5cd1d10123415beb526a1f9d9c6c5039c60194
iris263 <115156488+iris263@users.noreply.github.com>
2022-10-17 12:09:47 +0200

    Update implementations.py
    add ridge regression (attention, il faudra l'utiliser avec phi(x))

--- implementations.py
+++ implementations.py
@@ -73,3 +73,27 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma) :
         loss = compute_loss (y,tx,w)
        
     return w, loss
+
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression.
+    
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        lambda_: scalar.
+    
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 0)
+    array([ 0.21212121, -0.12121212])
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
+    array([0.03947092, 0.00319628])
+    """
+    
+    n = tx.shape[0]
+    d = tx.shape[1]
+    w = np.linalg.solve(np.dot(tx.T,tx) + lambda_*2*n * np.identity(d), np.dot(tx.T,y))
+    loss = compute_loss(y,tx,w)
+    
+    return w, loss
