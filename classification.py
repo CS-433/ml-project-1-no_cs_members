b37cc69b034c45ad80e3e7ff04f43603a2ac8b36
Mathilde Morelli <115156167+matou1604@users.noreply.github.com>
2022-10-28 18:02:59 +0200

    new_classification

--- classification.py
+++ classification.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from math import dist,ceil
 
 
+
 def get_Kneighbors(x_tr, K, new_sample):
     """Finds K nearest neighbors using euclidian distance
     
@@ -56,17 +57,19 @@ def get_prediction(Kindexes, K, y_tr):
     
     for m in range (K):
         predictions[m] = y_tr[np.int(Kindexes[m])]  #warning because of forced int()
-
-    if sum(predictions) >= ceil(K/2):
+        
+    scoreKNN = np.mean(predictions)
+        
+    if scoreKNN >= 0.5:
         new_prediction = 1
     else:
         new_prediction = 0
         
-    return predictions, new_prediction
+    return scoreKNN, new_prediction
 
 
 
-def get_accuracy(y_predictions, y_te):
+def get_accuracy(y_results, y_te, score):
     """Checks whether prediction are accurate by compraing with y_te
     
     Args: 
@@ -78,7 +81,7 @@ def get_accuracy(y_predictions, y_te):
         len(bad_guess):
     """ 
     
-    difference = (y_predictions-y_te)
+    difference = (y_results-y_te)
     good_guess = difference[difference==0]
     bad_guess = difference[difference!=0]
     accuracy = len(good_guess)/(len(good_guess)+len(bad_guess))
@@ -87,20 +90,74 @@ def get_accuracy(y_predictions, y_te):
     FP = 0
     FN = 0
     
-    for i in range(y_predictions.shape[0]):
+    for i in range(y_results.shape[0]):
         if difference[i] == 1:
             FP +=1
         if difference[i] == -1:
             FN +=1
         else :
-            if y_predictions[i] == 1:
+            if y_results[i] == 1:
                 TP +=1
             else:
-                TN +=1
+                TN +=1       
                 
     precision = TP/(TP+FP)  
     recall = TP/(TP+FN)
+    auc = get_auc(score, y_te)
     #print(f"How well our model can classify binary outcomes: accuracy of {accuracy}, precision of {precision}, and recall of {recall}")
-    print("How well our model can classify binary outcomes: accuracy of %.3f precision of %.3f, and recall of %.3f" % (accuracy, precision, recall))
+    print("How well our model can classify binary outcomes: accuracy of %.3f, precision of %.3f, recall of %.3f, and AUC score of %.3f" % (accuracy, precision, recall, auc))
     
     return accuracy, precision, recall
+
+
+
+def get_auc(score, y_results):
+
+    y = y_results
+
+    # false positive rate
+    FPR = []
+    # true positive rate
+    TPR = []
+    # Iterate thresholds from 0.0 to 1.0
+    thresholds = np.arange(0.0, 1.01, 0.001)
+    print(len(thresholds))
+
+    # get number of positive and negative examples in the dataset
+    P = sum(y)
+    N = len(y) - P
+
+    # iterate through all thresholds and determine fraction of true positives
+    # and false positives found at this threshold
+    for thresh in thresholds:
+        FP=0
+        TP=0
+        thresh = round(thresh,2) 
+        for i in range(len(score)):
+            if (score[i] >= thresh):
+                if y[i] == 1:
+                    TP += 1
+                if y[i] == 0:
+                    FP += 1            
+        FPR = np.append(FPR,FP/N)
+        TPR = np.append(TPR, TP/P)
+
+    #computing Arean Under Curve using the trapezoidal method
+    auc = -1 * np.trapz(TPR, x=FPR)
+    print(auc)
+
+    
+    plt.plot(FPR, TPR, marker='.', color='darkorange', label='ROC curve', clip_on=False)
+    plt.plot([0, 1], [0, 1], color='navy', linestyle='--', label = 'No Discrimination')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.title('ROC curve, AUC = %.2f'%auc)
+    plt.legend(loc="lower right")
+    plt.grid(True)
+    plt.savefig('AUC_example.png')
+    plt.show()
+    
+    return auc
+
